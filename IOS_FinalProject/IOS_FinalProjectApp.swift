c9164d4ac9304ca3a734427d9ce712cd27238883
Dylan <s89673827@gmail.com>
2021-04-28 10:52:02 +0800

    Initial Commit

---
+++ IOS_FinalProject/IOS_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_FinalProjectApp.swift
+//  IOS_FinalProject
+//
+//  Created by 朱冠儒 on 2021/4/28.
+//
+
+import SwiftUI
+
+@main
+struct IOS_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
